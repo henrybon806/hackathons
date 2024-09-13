6e81f70d7fcb3de3f8fd2d39114b0345411af8cb
Henry Bonomolo <hbono@berkeley.edu>
2024-09-12 21:42:02 -0700

    first commit

---
+++ jsFunc.js
@@ -0,0 +1,21 @@
+async function loadCollegesFromCSV() {
+    try {
+        const response = await fetch('schools.csv');
+        const csvText = await response.text();
+
+        const colleges = csvText.split('\n').map(college => college.trim()).filter(college => college !== '');
+        const collegeDropdown = document.getElementById('college');
+
+        colleges.forEach(function(college) {
+            const option = document.createElement('option');
+            option.value = college;
+            option.text = college;
+            collegeDropdown.appendChild(option);
+        });
+    } catch (error) {
+        console.error('Error loading CSV file:', error);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', loadCollegesFromCSV);
+console.log("test")
